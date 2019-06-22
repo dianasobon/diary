00378c47a6ad26fecb0b06084297b7e375ec4a53
dianasobon <dianasobon86@gmail.com>
2019-06-22 06:43:32 +0200

    Commit message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+.
+.
+//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
